48f7f9d0a6f111f7a65e1e177cca6befb3e7f977
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2019-09-20 17:08:35 +0200

    factorize mailbox_ortho

--- Mailbox_ortho.py
+++ Mailbox_ortho.py
@@ -20,27 +20,48 @@ mailbox_blue = MailboxDetector([[103, 129, 0],[129, 190, 255]], 1200, color="BLU
 mailbox_yellow = MailboxDetector([[0, 0, 0],[179, 6, 255]], 1500, aspect_ratio_th=0.6, color="YELLOW") # for test image only
 mailbox_orange = MailboxDetector([[141, 61, 0],[163, 76, 255]], 500, color="ORANGE")
 
+def process_result(img, out, res, label):
+    center = (int(res[0][0]), int(res[0][1]))
+    cv2.circle(out, center, 50, (0, 255, 0), 5)
+    #cv2.putText(out, label, (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+    box = cv2.cv.BoxPoints(res)
+    ctr = np.array(box).reshape((-1,1,2)).astype(np.int32)
+    mask = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+    cv2.drawContours(mask, [ctr], -1, (255,255,255),-1)
+    img = cv2.bitwise_and(img,img,mask = cv2.bitwise_not(mask))
+    return img, out
+
 def find_mailboxes(img, output=None, scale=DEFAULT_SCALE_FACTOR, res=DEFAULT_RESOLUTION):
     out = img.copy()
 
     scale_factor = pow(res / 1000., 2)
+
     res = mailbox_red.detect(img, scale_factor)
     if res is not None:
-        center = (int(res[0][0]), int(res[0][1]))
-        cv2.circle(out, center, 50, (0, 255, 0), 5)
-        cv2.putText(out, "RED", (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+        img, out = process_result(img, out, res, "RED")
 
     res = mailbox_blue.detect(img, scale_factor)
     if res is not None:
-        center = (int(res[0][0]), int(res[0][1]))
-        cv2.circle(out, center, 50, (0, 255, 0), 5)
-        cv2.putText(out, "BLUE", (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+        img, out = process_result(img, out, res, "BLUE")
 
     res = mailbox_yellow.detect(img, scale_factor)
     if res is not None:
         center = (int(res[0][0]), int(res[0][1]))
         cv2.circle(out, center, 50, (0, 255, 0), 5)
-        cv2.putText(out, "YELLOW", (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+        #cv2.putText(out, "YELLOW", (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+
+    res = mailbox_orange.detect(img, scale_factor)
+    if res is not None:
+        center = (int(res[0][0]), int(res[0][1]))
+        cv2.circle(out, center, 50, (0, 255, 0), 5)
+        #cv2.putText(out, "ORANGE", (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+        # add a mask on object
+        box = cv2.cv.BoxPoints(res)
+        ctr = np.array(box).reshape((-1,1,2)).astype(np.int32)
+        mask = np.zeros(img.shape, np.uint8)
+        np.zeros(img.shape, np.uint8)
+        cv.drawContours(mask, contours, -1, (255,255,255),1)
+        img = cv2.bitwise_and(img,img,mask = mask)
 
     if output is None:
         w, h, _ = img.shape
