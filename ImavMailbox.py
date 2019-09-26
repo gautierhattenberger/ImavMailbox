7d6c1dfad6b58c161e89448c2a665ecb1008d72f
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2019-09-26 22:24:45 +0200

    add fisheye calib and enable detectors

--- ImavMailbox.py
+++ ImavMailbox.py
@@ -23,6 +23,7 @@ class ImavMailbox:
         # cam params
         self.focal = (770., 770.)
         self.center = (320., 240.)
+        self.calib_fisheye = None
 
         self.save = None # save current image
 
@@ -69,16 +70,16 @@ class ImavMailbox:
             self.send_message(MARK_BLUE, ret)
 
         # yellow
-        #ret = self.mailbox_yellow.detect(img, size_factor)
-        #if ret is not None:
-        #    detect[MARK_YELLOW] = ret
-        #    self.send_message(MARK_YELLOW, ret)
+        ret = self.mailbox_yellow.detect(img, size_factor)
+        if ret is not None:
+            detect[MARK_YELLOW] = ret
+            self.send_message(MARK_YELLOW, ret)
 
         # orange (assuming only one in image)
-        #ret = self.mailbox_orange.detect(img, size_factor)
-        #if ret is not None:
-        #    detect[MARK_ORANGE] = ret
-        #    self.send_message(MARK_ORANGE, ret)
+        ret = self.mailbox_orange.detect(img, size_factor)
+        if ret is not None:
+            detect[MARK_ORANGE] = ret
+            self.send_message(MARK_ORANGE, ret)
 
         return detect
 
@@ -87,42 +88,55 @@ class ImavMailbox:
         send message over serial link to AP
         '''
         (u, v), (w, h), _ = pos
-        # pos in "mm" in image frame
-        x = 1000. * (u - self.center[0]) / self.focal[0]
-        y = 1000. * (v - self.center[1]) / self.focal[1]
+        x ,y = 0., 0.
+        if self.calib_fisheye is None:
+            # pos in "mm" in image frame
+            x = 1000. * (u - self.center[0]) / self.focal[0]
+            y = 1000. * (v - self.center[1]) / self.focal[1]
+        else:
+            pts_uv = np.array([[[u, v]]], dtype=np.float32)
+            undist = cv2.fisheye.undistortPoints(pts_uv, self.calib_fisheye[0], self.calib_fisheye[1])
+            x = 1000. * undist[0][0][0]
+            y = 1000. * undist[0][0][1]
         jevois.sendSerial("N2 {} {:.2f} {:.2f} {:.2f} {:.2f}".format(mark, x, y, w, h))
 
     def parseSerial(self, cmd):
         str_list = cmd.split(' ')
-        if len(str_list) == 2 and str_list[0] == "alt" and str_list[1].isdigit():
+        str_len = len(str_list)
+        if str_len == 2 and str_list[0] == "alt" and str_list[1].isdigit():
             self.alt = int(str_list[1])
             return "OK"
-        elif len(str_list) == 2 and str_list[0] == "save":
+        elif str_len == 2 and str_list[0] == "save":
             self.save = "/jevois/data/images/{}.png".format(str_list[1])
             return self.save
-        elif len(str_list) == 7 and str_list[0] == "hsv_red":
+        elif str_len == 7 and str_list[0] == "hsv_red":
             h_min = [int(str_list[1]), int(str_list[2]), int(str_list[3])]
             h_max = [int(str_list[4]), int(str_list[5]), int(str_list[6])]
             self.mailbox_red.set_hsv_th(h_min, h_max)
             return "OK"
-        elif len(str_list) == 7 and str_list[0] == "hsv_blue":
+        elif str_len == 7 and str_list[0] == "hsv_blue":
             h_min = [int(str_list[1]), int(str_list[2]), int(str_list[3])]
             h_max = [int(str_list[4]), int(str_list[5]), int(str_list[6])]
             self.mailbox_blue.set_hsv_th(h_min, h_max)
             return "OK"
-        elif len(str_list) == 7 and str_list[0] == "hsv_yellow":
+        elif str_len == 7 and str_list[0] == "hsv_yellow":
             h_min = [int(str_list[1]), int(str_list[2]), int(str_list[3])]
             h_max = [int(str_list[4]), int(str_list[5]), int(str_list[6])]
             self.mailbox_yellow.set_hsv_th(h_min, h_max)
             return "OK"
-        elif len(str_list) == 7 and str_list[0] == "hsv_orange":
+        elif str_len == 7 and str_list[0] == "hsv_orange":
             h_min = [int(str_list[1]), int(str_list[2]), int(str_list[3])]
             h_max = [int(str_list[4]), int(str_list[5]), int(str_list[6])]
             self.mailbox_orange.set_hsv_th(h_min, h_max)
             return "OK"
-        elif len(str_list) == 5 and str_list[0] == "calib":
+        elif str_len == 5 and str_list[0] == "calib":
             self.focal = (float(str_list[1]), float(str_list[2]))
             self.center = (float(str_list[3]), float(str_list[4]))
+            return "OK"
+        elif str_len == 9 and str_list[0] == "calib_fisheye":
+            K = np.array([[float(str_list[1]), 0., float(str_list[3])], [0., float(str_list[2]), float(str_list[4])]])
+            D = np.array([[float(str_list[5])], [float(str_list[6])], [float(str_list[7])], [float(str_list[8])]])
+            self.calib_fisheye = (K, D)
             return "OK"
         return "ERR"
 
